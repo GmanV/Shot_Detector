010d9e7109e132523984dad86de603c31b08db1f
GmanV <gerrit.j.vreman@intel.com>
2016-04-26 11:58:54 -0700

    Update shot_detect_3.py

--- shot_detect_3.py
+++ shot_detect_3.py
@@ -5,7 +5,7 @@ import socket   #for sockets
 import sys  #for exit
 
 INTERVAL = .025                # Sound Signature Sample time
-BYSEC_INTERVAL =5
+BYSEC_INTERVAL =20
 SENDMSG_INTERVAL =60           # Minimum time between sending something to cloud
 
 #LED_GPIO = 5                   # The LED pin
